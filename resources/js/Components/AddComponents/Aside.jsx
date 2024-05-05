691bd5189cc176bd3a21fcb5f70b0b04b3151d7a
shofiyul fuad <shofiyulfuad797@gmail.com>
2024-05-05 08:41:05 +0700

    pending

--- resources/js/Components/AddComponents/Aside.jsx
+++ resources/js/Components/AddComponents/Aside.jsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { CiSearch } from "react-icons/ci";
 import { IoIosArrowDown } from "react-icons/io";
 import { useState } from "react";
--- resources/js/Components/AddComponents/MainUserSetting.jsx
+++ resources/js/Components/AddComponents/MainUserSetting.jsx
@@ -2,7 +2,7 @@ const MainUserSetting = ({ auth }) => {
     return (
         <div className="flex flex-col gap-5 p-5 w-full border border-slate-200 rounded-lg shadow-lg">
             <div className="flex justify-between">
-                <h1 className="font-bold text-2xl">Setting</h1>
+                <h1 className="font-bold text-2xl">Setting pending</h1>
             </div>
         </div>
     );
